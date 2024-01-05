6c6c5c70361a3c4df4873cc7378422009fb5fe43
jui <juiyezarkar1999@gmail.com>
2024-01-06 00:08:02 +0530

    feat:created header and home files

---
+++ src/screens/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className=''>index</div>
+  )
+}
+
+export default Header
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Header from '../Header'
 
 const Home = () => {
   return (
-    <div className=' font-sans text-7xl text-pink-950'>info</div>
+    <>
+    <Header></Header>
+    <div className='font-sans text-7xl text-pink-950'>info</div>
+    </>
   )
 }
 
